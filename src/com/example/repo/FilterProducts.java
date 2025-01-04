fe9a8555c6ffa3b2d2cc3f72dd7fed389b224e9e
kaustubh_choudhary <kaustubhchoudhary@yahoo.com>
2025-01-05 00:21:24 +0530

    Implemented methods to get filtered products

---
+++ src/com/example/repo/FilterProducts.java
@@ -0,0 +1,21 @@
+package com.example.repo;
+
+import java.util.Arrays;
+
+import com.example.entities.Product;
+
+public class FilterProducts {
+	public static Product[] getAvailableProducts() {
+		
+		Product[] products = new Product[5];
+
+		products[0] = new Product(101, "TV", 20000, 4, true);
+		products[1] = new Product(102, "Watch", 5000, 5, false);
+		products[2] = new Product(103, "Mobile", 22000, 4, true);
+		products[3] = new Product(104, "Fridge", 25000, 3, true);
+		products[4] = new Product(105, "Oven", 12000, 2, false);
+
+		return Arrays.stream(products).filter(Product::isInStock)
+        .toArray(Product[]::new);
+	}
+}
