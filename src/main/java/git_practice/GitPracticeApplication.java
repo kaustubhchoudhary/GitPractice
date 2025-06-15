6519bbcfa0605bc020fd88a1acc0f534694b3d45
kaustubhchoudhary <kaustubhchoudhary@yahoo.com>
2025-06-15 19:35:04 +0530

    Initial Commit

---
+++ src/main/java/git_practice/GitPracticeApplication.java
@@ -0,0 +1,14 @@
+package git_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPracticeApplication.class, args);
+		System.out.println("Yo bro");
+	}
+
+}
---
+++ src/test/java/GitPractice/GitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package GitPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
